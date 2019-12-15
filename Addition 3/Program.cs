16e4a87d1e52a5cf8a60a863b21248db5de2463e
JulPsh <6523+JulPsh@users.noreply.github.com>
2019-12-15 22:48:30 +0300

    Addition-3

---
+++ Addition 3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Addition_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = "Меня";
+            string s2 = "зовут";
+            string s3 = "Юля";
+            string s4 = s1 + " " + s2 + " " + s3;
+            Console.WriteLine(s4);
+        }
+    }
+}
